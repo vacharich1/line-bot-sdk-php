0d5ff1bc6bd23746631a7ac54f1c2b67a4b93b45
jay <jr30149_@hotmail.com>
2016-10-19 19:08:11 +0700

    ssssss
    asdfdsfsdf

--- line-bot-sdk-tiny/echo_bot.php
+++ line-bot-sdk-tiny/echo_bot.php
@@ -18,8 +18,8 @@
 
 require_once('./LINEBotTiny.php');
 
-$channelAccessToken = '<your channel access token>';
-$channelSecret = '<your channel secret>';
+$channelAccessToken = 'AYydB5m2TZasBEFQaZjNRTCTeC3d3oNKw77jzKd/mj3SAMlkABDK74AAJ6eN00no1+MiFoFV2N5pl1KIYZmlq8/WSmxf2b4WVhcvfjJoUH6TY6AZoQrYmAP/ny8krS0KwSMDOokFaUouicUyyIKmhQdB04t89/1O/w1cDnyilFU=';
+$channelSecret = '3692fbc3db90c226b12e3f91130e2f9f';
 
 $client = new LINEBotTiny($channelAccessToken, $channelSecret);
 foreach ($client->parseEvents() as $event) {
